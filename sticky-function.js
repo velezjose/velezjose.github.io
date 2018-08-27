d0d2e135cf0e1e2ed9401712b746d1efb4871f1c
Jose Velez <josevelezq7@gmail.com>
2018-08-27 18:14:28 -0400

    Changed layout, CSS, file organization, and HTML.

---
+++ sticky-function.js
@@ -0,0 +1,15 @@
+    <script>
+        window.onscroll = function() {stickyOnTop()};
+
+        var header = document.getElementById("entire-website-header");
+        var sticky = header.offsetTop;
+
+        function stickyOnTop() {
+            if (window.pageYOffset >= sticky) {
+                header.classList.add("sticky");
+            } else {
+                header.classList.remove("sticky");
+            }
+        }
+
+    </script>
