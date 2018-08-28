4867ed74b4ab3369664a461227e2cb02796a2061
Jose Velez <josevelezq7@gmail.com>
2018-08-27 21:47:36 -0400

    Think I got it right this time. Also changed README

--- scripts/load-header-and-footer-main.js
+++ scripts/load-header-and-footer-main.js
@@ -1,4 +1,4 @@
 $(document).ready(function(){
-    $("#entire-website-header").load("../reuseheader.html #header");
-    $("#footer").load("../reuseheader.html #footer");
+    $("#entire-website-header").load("reuseheader.html #header");
+    $("#footer").load("reuseheader.html #footer");
 });
