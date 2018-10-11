98941490e501b0314edd251562ad6cb2bc515569
Jose Velez <josevelezq7@gmail.com>
2018-10-11 02:07:15 -0400

    Lots of changes:
    - Import meta info instead of having it copied and pasted for every page.
    - Change layout of the header bar
    - Change nav bar links
    - Shrink topnav to hamburger when width < 900px

--- scripts/load-header-and-footer.js
+++ scripts/load-header-and-footer.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $('#entire-website-header').load('../reuseheader.html #header');
+    $('#entire-website-header').load('../reuseheader.html #myTopnav');
     $('#footer').load('../reuseheader.html #footer');
     $('#mySidenav').load('../reuseheader.html #mySidenav');
 });
