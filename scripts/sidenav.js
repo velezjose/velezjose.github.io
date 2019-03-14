cd49e27a95378d9724733d7b2a907ed36a1540f3
Jose Velez <josevelezq7@gmail.com>
2019-03-13 22:39:35 -0700

    Make the sidebar hamburger navigation work for mobile responsivity

--- scripts/sidenav.js
+++ scripts/sidenav.js
@@ -1,6 +1,20 @@
+$( window ).resize(() => {
+    let w = $( window ).width();
+    let sideNavHidden = $('#sidenav').prop('hidden');
+    let mySideNavHidden = $('#mySidenav').prop('hidden');
+
+    if (w <= 949 && sideNavHidden === true) {
+        $('#sidenav').prop('hidden', false);
+    }
+
+    if (w > 949 && sideNavHidden === false) {
+        $('#sidenav').prop('hidden', true);
+    }
+});
+
 function openNav() {
-    $('#mySidenav').css('width', '250px');
     $('#mySidenav').css('display', 'block');
+    $('#mySidenav').css('width', '250px');
 }
 
 function closeNav() {
