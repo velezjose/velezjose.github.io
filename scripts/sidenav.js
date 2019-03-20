771d0bd4955482abda805a39e508c141ab906332
Jose Velez <josevelezq7@gmail.com>
2019-03-19 22:47:12 -0700

    Fix sidenav rendering for mobile screenwidths

--- scripts/sidenav.js
+++ scripts/sidenav.js
@@ -1,4 +1,5 @@
-$( window ).resize(() => {
+
+$( document ).ready(() => {
     let w = $( window ).width();
     let sideNavHidden = $('#sidenav').prop('hidden');
     let mySideNavHidden = $('#mySidenav').prop('hidden');
@@ -10,7 +11,23 @@ $( window ).resize(() => {
     if (w > 949 && sideNavHidden === false) {
         $('#sidenav').prop('hidden', true);
     }
+
+    $( window ).resize(() => {
+        let w = $( window ).width();
+        let sideNavHidden = $('#sidenav').prop('hidden');
+        let mySideNavHidden = $('#mySidenav').prop('hidden');
+    
+        if (w <= 949 && sideNavHidden === true) {
+            $('#sidenav').prop('hidden', false);
+        }
+    
+        if (w > 949 && sideNavHidden === false) {
+            $('#sidenav').prop('hidden', true);
+        }
+    });
+    
 });
+
 
 function openNav() {
     $('#mySidenav').css('display', 'block');
